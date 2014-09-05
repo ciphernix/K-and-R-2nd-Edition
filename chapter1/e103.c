bbdcdff3302a701a145fc892f410f43a1fde2d60
ciphernix <ciphernix@gmail.com>
2014-09-05 06:55:03 -0400

    Scrippted commit

--- chapter1/e103.c
+++ chapter1/e103.c
@@ -14,19 +14,19 @@ int main(void)
 {
 	float fahr, celsius;
     float lower, upper, step;
-	
-    lower = 0;      /* lower limit of temperatuire scale */
-    upper = 300;    /* upper limit */
-    step = 20;      /* step size */
-	
-    fahr = lower;
+
+	lower = 0;      /* lower limit of temperatuire scale */
+	upper = 300;    /* upper limit */
+	step = 20;      /* step size */
+
+	fahr = lower;
 	printf("Fahrenheit-Celsius table\n");
 	printf("Fahrenheit\tCelsius\n");
 	printf("----------\t--------\n");
-    while (fahr <= upper) 
+	while (fahr <= upper) 
 	{
-    	celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f \t%6.1f\n", fahr, celsius);
+ 		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f \t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
 	}
 	
