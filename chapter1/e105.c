8deebf7c3f27deff3706032445c496acc86b1f28
ciphernix <ciphernix@gmail.com>
2014-09-06 08:57:01 -0400

    e105

--- chapter1/e105.c
+++ chapter1/e105.c
@@ -10,5 +10,15 @@ int main(void)
 {
 	float celsius, fahr;
 	
-	for 
-	celsius = (5.0/9.0) * (fahr-32.0);
+	printf("Fahrenheit-Celsius table\n");
+	printf("Fahrenheit\tCelsius\n");
+	printf("-----------\t-------\n");
+	
+	for (fahr = 300.0; fahr >= 0.0; fahr -= 20.0)
+	{
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %15.1f\n", fahr, celsius);
+	}
+	return 0;
+}
+
