be8eca77f7c5aae5dcdcc3c5e336c41aac1d2813
ciphernix <ciphernix@gmail.com>
2014-09-05 20:06:02 -0400

    E105.c

---
+++ chapter1/e105.c
@@ -0,0 +1,14 @@
+/*
+ * Exercise 1-5:
+ * Modify the temperature conversion program to print the table in reverse
+ * order, that is, from 300 degrees to 0.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	float celsius, fahr;
+	
+	for 
+	celsius = (5.0/9.0) * (fahr-32.0);
