829a32ef2988803022a60580edba71bfc08c0c9c
ciphernix <ciphernix@gmail.com>
2014-09-23 16:49:49 -0400

    Fixed e117.c

--- chapter1/e117.c
+++ chapter1/e117.c
@@ -2,51 +2,50 @@
  * Exercise 1-17: 
  * Write a program to print all input lines that are longer than 80 characters.
  */
-
 #include <stdio.h>
 
-#define MAXLINE 81   /* maximum input line length */
+#define MAXLINE 81	/* maximum input line length */
 
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
+int getlines(char line[], int maxline);
+void copy(char to[], char from[]);
 
 /* print lines longer than MAXLINE */
 
 int main(void)
 {
-	int len;            /* current line length */
-	char line[MAXLINE];    /* current input line */
-	char c; 
+	int len;	/* current line length */
+	char line[MAXLINE];	/* current input line */
+	char c;
 	
 
-	while ((len = getline(line, MAXLINE)) > 0)
+	while ((len = getlines(line, MAXLINE)) > 0)
 	{
-		if (len == MAXLINE-1 && line[len-1] != '\n')  //line overflows
+		if (len == MAXLINE-1 && line[len-1] != '\n') //line overflows
 		{	
 			printf("%s", line);
-			while (c=getchar())!=EOF && c!='\n')
+			while ((c=getchar())!=EOF && c!='\n')
 				putchar(c);
 			
 			if (c == '\n')
-				putchar('\n')
+				putchar('\n');
 		}
 
 	}
 	return 0;
 }
 
-/* getline:  read a line into s, return length  */
-int getline(char s[],int lim)
+/* getlines:  read a line into s, return length  */
+int getlines(char s[],int lim)
 {
-	int c, i;
+	int c, i;
 	
-	for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-		s[i] = c;
-	if (c == '\n')
+	for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c == '\n')
 	{
-		s[i] = c;
+		s[i] = c;
 		++i;
 	}
-	s[i] = '\0';
-	return i;
+	s[i] = '\0';
+	return i;
 }
