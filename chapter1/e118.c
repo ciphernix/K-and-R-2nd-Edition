f009b232dbb7224929fc95935697a5b12fd03023
ciphernix <ciphernix@gmail.com>
2014-09-25 04:45:07 -0400

    fixed e118

--- chapter1/e118.c
+++ chapter1/e118.c
@@ -7,42 +7,37 @@
 
 #define MAXBUFFER 1001
 
-int getline(char buffer[], int lim);
-
-int getline(char buffer[], int lim)
-{
-	int c, i;
-	
-	i = 0;
-	while ((c = getchar()) != EOF && c != '\n' && i < lim - 1 )
-	{
-		if (c != ' ' && c != '\t' )
-		{
-			buffer[i] = c;
-			++i;
-		}
-	}
-	
-	buffer[i] = '\n';
-	++i;
-	
-	buffer[i] = '\0';
-	
-	if (c == EOF )
-		return EOF;
-	else 
-		return i;
-}
-
 int main (void)
 {
 	char buffer[MAXBUFFER];
-	
-	while (getline(buffer, MAXBUFFER) != EOF)
+	int c;
+	int i = 0, non_blank = 0;
+
+	while ((c = getchar() ) != EOF )
 	{
-		if ( buffer[0] != '\n') //only print non empty line
-			printf("%s",buffer);
+		if ( c == ' ' || c == '\t' )
+		{
+			buffer[i] = c;
+			++i;
+			non_blank = 0;
+		}
+		else if ( c != '\n' )
+		{
+			buffer[i] = '\0';
+			printf("%s", buffer);
+			putchar(c);
+			non_blank = 1;
+			i = 0;
+		}
+		else if (non_blank) // print  new line if non_blank = 1
+		{
+			putchar(c);
+			i = 0;
+			non_blank = 0;
+		}
+
 	}
 	
 	return 0;
-}
+}                    							
+
